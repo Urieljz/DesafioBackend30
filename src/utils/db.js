ac4a5c90266df4ba07bbcfa158864b3148499129
geranhuns <gera.nuncio@gmail.com>
2024-02-06 21:50:59 -0600

    utis

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose') 
 //Nota: cambiar despues del @ el string de conexion de la base de datos local
-const URI = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.soojv6g.mongodb.net/${process.env.NAME_DB}`
+const URI = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.tznp10q.mongodb.net/${process.env.NAME_DB}`
 async function connect() {
 
     try {
