40fb0a8c222dff04a16639660a80c23d43423a76
gdHermosillo <gadiel.ddelgado@gmail.com>
2024-02-07 21:56:27 -0600

    ya quedó

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,32 +1,33 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const JWT_SIGN = process.env.JWT_SIGN;
-const User = require('../models/users');
+const User = require("../models/users");
 
 function createJWT(data) {
-    return jwt.sign(data, JWT_SIGN, { expiresIn: '1h' });
+  return jwt.sign(data, JWT_SIGN, { expiresIn: "1h" });
 }
 
 function verifyJWT(req, res, next) {
-    const token = req.headers.authorization;
-    const dateNow = new Date();
+  let token = req.headers.authorization;
+  const dateNow = new Date();
 
-    if (!token) {
-        return res.status(401).send({ msg: 'Login required' });
+  if (!token) {
+    return res.status(401).send({ msg: "Login required" });
+  }
+
+  token = token.split(" ")[1];
+  jwt.verify(token, JWT_SIGN, async (err, decoded) => {
+    console.log(token);
+    if (err) {
+      return res.status(401).send({ msg: "Invalid token" });
     }
 
-    token = token.split(' ')[1];
-    jwt.verify(token, JWT_SIGN, async (err, decoded) => {
-        if (err) {
-            return res.status(401).send({ msg: 'Invalid token' });
-        }
-
-        if (decoded.exp < dateNow.getTime() / 1000) {
-            return res.status(401).send({ msg: 'Expired token' });
-        } else {
-            req.user = await User.findById(decoded._id);
-            next();
-        }
-    });
+    if (decoded.exp < dateNow.getTime() / 1000) {
+      return res.status(401).send({ msg: "Expired token" });
+    } else {
+      req.user = await User.findById(decoded._id);
+      next();
+    }
+  });
 }
 
 module.exports = { createJWT, verifyJWT };
